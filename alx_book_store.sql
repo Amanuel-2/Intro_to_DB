8a4c7281e00ffc593c1025272d0888695612f5ad
Amanuel <amanuelwendimu19@gmail.com>
2025-12-13 22:29:46 +0300

    fix debbug-1

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,41 +1,34 @@
--- Create the database
-CREATE DATABASE alx_book_store;
--- Select the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
--- Authors table
-CREATE TABLE Authors (
-    author_id INT PRIMARY KEY AUTO_INCREMENT,
-    author_name VARCHAR(215) NOT NULL
+CREATE TABLE Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
 );
--- Books table
-CREATE TABLE Books (
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
-    price DOUBLE NOT NULL,
+    Price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id)
 );
--- Customers table
-CREATE TABLE Customers (
-    customer_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
--- Orders table
-CREATE TABLE Orders (
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    order_date DATE,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id)
 );
--- Order_Details table
-CREATE TABLE Order_Details (
-    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Order_Details(
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    quantity DOUBLE,
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id)
 );
