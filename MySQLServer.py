3f02c07e9aa33c153b8fd893055eef174617b08e
Amanuel <amanuelwendimu19@gmail.com>
2025-12-14 22:26:19 +0300

    fix server and tasks

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,18 +1,21 @@
 import mysql.connector
 from mysql.connector import Error
-try:
-    mydb = mysql.connector.connect(
-        host = "localhost",
-        user = "root",
-        passwd = "mysql@1920"
-    )
-    if mydb.is_connected():
-        mycursor = mydb.cursor()
-        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("database 'alx_book_store' created successfully!")
-except Error as e:
-    print("Error while connecting to MySQL",e)
-finally:
-    if mydb.is_connected():
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            passwd = "mysql@1920"
+        )
+        if connection.is_connected():
+            mycursor = connection.cursor()
+            mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+    except Error:
+     print(Error)
+    finally:
+     if connection.is_connected():
         mycursor.close()
-        mydb.close()
+        connection.close()
+if __name__== "__main__":git 
+   create_database()
--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
-INSERT INTO customer (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (
         2,
         'Blessing Malik',
