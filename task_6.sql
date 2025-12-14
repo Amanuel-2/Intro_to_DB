fc70faa0b1e43b903a774771830bad1e97673de8
Amanuel <amanuelwendimu19@gmail.com>
2025-12-14 23:02:42 +0300

    fix Errors

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (
         2,
         'Blessing Malik',
