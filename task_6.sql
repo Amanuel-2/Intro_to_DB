34ad924846849e53bb28ce2e8e0c99c71f5b88bb
Amanuel <amanuelwendimu19@gmail.com>
2025-12-14 21:56:38 +0300

    create task

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES (
         2,
         'Blessing Malik',
