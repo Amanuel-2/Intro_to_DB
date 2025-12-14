6d9a4ac635d05d17d270ac3c1e65fac015f7b866
Amanuel <amanuelwendimu19@gmail.com>
2025-12-14 22:42:18 +0300

    fix all

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,21 @@
 import mysql.connector
 from mysql.connector import Error
-def create_database():
-    try:
-        connection = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            passwd = "mysql@1920"
-        )
-        if connection.is_connected():
-            mycursor = connection.cursor()
-            mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-            print("Database 'alx_book_store' created successfully!")
-    except Error:
-     print(Error)
-    finally:
-     if connection.is_connected():
-        mycursor.close()
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="mysql@1920"
+    )
+
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print("Error while connecting to MySQL:", e)
+
+finally:
+    if connection.is_connected():
+        cursor.close()
         connection.close()
-if __name__== "__main__":git 
-   create_database()
